6b95e7cbe81a11ca1fd94ec2f42a068b0940373d
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-01-20 16:59:43 +0100

    tracking-tab style, tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TrackingTabComponent } from './tracking-tab/tracking-tab.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tracking-tab/tracking-tab.component.ts
+++ src/app/tracking-tab/tracking-tab.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tracking-tab.component.scss']
 })
 export class TrackingTabComponent implements OnInit {
-
+  tableBtnInactive: boolean = false;
+  chartBtnInactive: boolean = true;
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  activeBtn(btn: string) {
+    if(btn === 'table') {
+      this.tableBtnInactive = false;
+      this.chartBtnInactive = true;
+    } 
+    
+    if(btn === 'chart')  {
+      this.tableBtnInactive = true;
+      this.chartBtnInactive = false;
+    }
   }
 
 }
