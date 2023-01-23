8a3def6cf1361aab133450ac49181630e3349e02
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-01-23 18:04:10 +0100

    first info in chart

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CoinsAPIService } from '../services/coins-api.service';
 
 @Component({
   selector: 'app-chart',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service: CoinsAPIService) { }
 
   ngOnInit(): void {
   }
--- src/app/tracking-tab/tracking-tab.component.ts
+++ src/app/tracking-tab/tracking-tab.component.ts
@@ -7,26 +7,11 @@ import { CoinsAPIService } from '../services/coins-api.service';
   styleUrls: ['./tracking-tab.component.scss']
 })
 export class TrackingTabComponent implements OnInit {
-  tableBtnInactive: boolean = false;
-  chartBtnInactive: boolean = true;
+  tableBtnInactive: boolean = true;
+  chartBtnInactive: boolean = false;
 
   
   constructor(public service: CoinsAPIService) { }
 
-  ngOnInit(): void {
-  }
-
-
-  activeBtn(btn: string) {
-    if (btn === 'table') {
-      this.tableBtnInactive = false;
-      this.chartBtnInactive = true;
-    }
-
-    if (btn === 'chart') {
-      this.tableBtnInactive = true;
-      this.chartBtnInactive = false;
-    }
-  }
-
+  ngOnInit(): void { }
 }
