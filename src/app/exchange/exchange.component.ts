6ba130c5df5d5b5f77a5af331d0dd3d97a691f7f
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-02-12 17:42:05 +0100

    add diverse exchange curreny

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -12,23 +12,55 @@ export class ExchangeComponent implements OnInit {
   today = new Date();
   firstCoin: any = '';
   secondCoin: any = '';
+  currencyFirstCoin: any = '';
+  currencySecondCoin: any = '';
   value: number = 1;
   activeChange: boolean = true;
+  currencyActiveChange: boolean = true;
+  currencyList = ['eth', 'bnb', 'btc', 'usd', 'gbp'];
+  coinIndex: any;
+  choosenCoin: any = '';
 
 
   constructor(public service: CoinsAPIService, public dialogRef: MatDialogRef<ExchangeComponent>) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
 
-  changedFirstValue(value: any){
+  chooseCoin(coin) {
+    this.choosenCoin = coin;
+    console.log(this.choosenCoin);
+    
+  }
+
+
+  changedFirstValue(value: any) {
     let result = value.target.value * this.service.firstExChangeCoin;
-    result > 100 ? this.firstCoin = result.toFixed(2).toString().replace('.', ',') : this.firstCoin = result.toFixed(10).toString().replace('.', ',');
+    this.firstCoin = result;
+    // result > 100 ? this.firstCoin = result.toFixed(2).toString().replace('.', ',') : this.firstCoin = result.toFixed(10).toString().replace('.', ',');
   }
 
 
-  changedSecondValue(value: any){
+  changedSecondValue(value: any) {
     let result = value.target.value / this.service.firstExChangeCoin;
-    result > 100 ? this.secondCoin = result.toFixed(2).toString().replace('.', ',') : this.secondCoin = result.toFixed(10).toString().replace('.', ',');
+    this.secondCoin = result;
+    // result > 100 ? this.secondCoin = result.toFixed(2).toString().replace('.', ',') : this.secondCoin = result.toFixed(10).toString().replace('.', ',');
   }
+
+
+  currencyChangedFirstValue(value: any, coin: any, i) {
+    this.coinIndex = i;
+    let result = value.target.value * this.service.clickedCoin.market_data.current_price[coin];
+    this.currencyFirstCoin = result;
+    // result > 100 ? this.currencyFirstCoin = result.toFixed(2).toString().replace('.', ',') : this.currencyFirstCoin = result.toFixed(10).toString().replace('.', ',');
+  }
+
+
+  currencyChangedSecondValue(value: any, coin: any, i) {
+    this.coinIndex = i;
+    let result = value.target.value / this.service.clickedCoin.market_data.current_price[coin];
+    this.currencySecondCoin = result;
+    // result > 100 ? this.currencySecondCoin = result.toFixed(2).toString().replace('.', ',') : this.currencySecondCoin = result.toFixed(10).toString().replace('.', ',');
+  }
+
 }
